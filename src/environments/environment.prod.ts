acf9f4fe8853956bb46b69148879bd91101b5c2e
Bereket Zelalem <Bereket.Zelalem@safaricom.et>
2023-04-26 16:59:12 +0300

    More Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,21 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyA-k8jG8nOTlyjM4WiI_I6yHVKZavLnmfA",
-    authDomain: "reminder-et.firebaseapp.com",
-    projectId: "reminder-et",
-    storageBucket: "reminder-et.appspot.com",
-    messagingSenderId: "699965676102",
-    appId: "1:699965676102:web:ee9423416c5540e2fb4847",
-    measurementId: "G-0FV189KSBK"
+    // apiKey: "AIzaSyA-k8jG8nOTlyjM4WiI_I6yHVKZavLnmfA",
+    // authDomain: "reminder-et.firebaseapp.com",
+    // projectId: "reminder-et",
+    // storageBucket: "reminder-et.appspot.com",
+    // messagingSenderId: "699965676102",
+    // appId: "1:699965676102:web:ee9423416c5540e2fb4847",
+    // measurementId: "G-0FV189KSBK"
+
+
+    apiKey: "AIzaSyDZ30J5eCtWEWW3rJoyNu6Je7hP4X1PDy4",
+    authDomain: "health-app-dashboard.firebaseapp.com",
+    projectId: "health-app-dashboard",
+    storageBucket: "health-app-dashboard.appspot.com",
+    messagingSenderId: "840857524719",
+    appId: "1:840857524719:web:6792717ae9958d5b537e1a",
+    measurementId: "G-D74SL3F97K"
 }
 }
