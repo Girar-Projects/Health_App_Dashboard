a097a435decdd86de35bc8a72ebd33557de1ed3a
Bereket Zelalem <Bereket.Zelalem@safaricom.et>
2023-04-26 17:00:30 +0300

    Firebase API KEY change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,6 @@ export const environment = {
   // appId: "1:699965676102:web:ee9423416c5540e2fb4847",
   // measurementId: "G-0FV189KSBK"
 
-  
   apiKey: "AIzaSyDZ30J5eCtWEWW3rJoyNu6Je7hP4X1PDy4",
   authDomain: "health-app-dashboard.firebaseapp.com",
   projectId: "health-app-dashboard",
@@ -19,6 +18,7 @@ export const environment = {
   messagingSenderId: "840857524719",
   appId: "1:840857524719:web:6792717ae9958d5b537e1a",
   measurementId: "G-D74SL3F97K"
+  
   }
 };
 
